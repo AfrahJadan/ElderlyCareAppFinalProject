1b8795c2fae1d09a84a124fd794f6502a7bed2d2
Afrah <Afrah.suliman.ruh.k@tuwaiq.edu.sa>
2021-12-27 16:11:57 +0300

    Initial commit

---
+++ app/src/main/java/com/afrahjadan/elderlycareapp/HomeActivity.kt
@@ -0,0 +1,39 @@
+package com.afrahjadan.elderlycareapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.afrahjadan.elderlycareapp.databinding.ActivityHomeBinding
+import com.google.firebase.auth.FirebaseAuth
+
+class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityHomeBinding
+    private lateinit var firebaseAuth:FirebaseAuth
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding= ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth =FirebaseAuth.getInstance()
+        checkUser()
+
+        binding.logoutBtn.setOnClickListener {
+            firebaseAuth.signOut()
+            checkUser()
+        }
+    }
+
+    private fun checkUser() {
+       val firebaseUser = firebaseAuth.currentUser
+        if(firebaseUser == null){
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
+        else{
+            val email = firebaseUser.email
+            binding.emailTv.text =email
+        }
+
+    }
+}

82133afb0eae42b08b7bd246a20753c92756f9f2
Afrah <Afrah.suliman.ruh.k@tuwaiq.edu.sa>
2022-01-21 00:13:03 +0300

    use function that upload google account picture in profile

--- app/src/main/java/com/afrahjadan/elderlycareapp/fragment/ProfileFragment.kt
+++ app/src/main/java/com/afrahjadan/elderlycareapp/fragment/ProfileFragment.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import android.icu.text.SimpleDateFormat
 import android.icu.util.Calendar
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -16,8 +15,6 @@ import com.afrahjadan.elderlycareapp.R
 import com.afrahjadan.elderlycareapp.databinding.FragmentProfileBinding
 import com.bumptech.glide.Glide
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
 import java.util.*
 
 
